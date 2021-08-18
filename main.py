6944785cb33f4618e6a652c8e7a2e75f07f17671
sebastian.ukleja <sebastian.ukleja@fh-campuswien.ac.at>
2021-08-18 13:13:09 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+class BinaryTree:
+
+    def __init__(self, data=None):
+        pass
+
+    def insert(self, data):
+        pass
+
+    #root - left - right
+    def preorder(self,node):
+        pass
+
+    #left - right -root
+    def postorder(self, node):
+        pass
+
+    #left - root -right
+    def inorder(self, node):
+        pass
+
+
+'''
+#You can use this code to test your tree:
+
+root=BinaryTree(10)
+root.insert(6)
+root.insert(8)
+root.insert(20)
+root.insert(3)
+root.insert(22)
+print("Postorder:")
+root.postorder(root)
+print("Preorder:")
+root.preorder(root)
+print("Inorder:")
+root.inorder(root)
+'''
